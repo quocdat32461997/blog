96f9a89cdd8151425e05b355377bc47158516d9e
quocdat32461997 <quocdat32461997@yahoo.com>
2019-09-01 22:34:58 -0500

    Update dynamodb config

---
+++ dynamodb_config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	aws_table_name: 'Recipes',
+	aws_local_config: {
+		region: 'local',
+		endpoint: 'http://localhost:8081'
+	},
+	aws_remote_config: {
+		region: 'us-east-1'
+	}
+};
