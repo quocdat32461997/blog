47641ccc43bf0326558f3107536236011a9889a0
quocdat32461997 <quocdat32461997@yahoo.com>
2019-09-08 12:55:05 -0500

    Updates on views and load-more feature

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,30 +1,31 @@
 const aws = require('aws-sdk');
 const aws_config = require('../dynamodb_config.js');
 var recipeCount = 0;
-const last_key = null;
+var last_key = null;
 
 //Configure aws dynamodb
 aws.config.update(aws_config.aws_remote_config);
+const document = new aws.DynamoDB.DocumentClient();
 /*
 * displayRecipesInHomePage - function to query recipes and display recipes in home page
 */
 exports.displayRecipesInHomePage = async function(req, res) {
-	var document = new aws.DynamoDB.DocumentClient();
+	//params - store TableName and Limit number of recipes displayed to screen
 	var params = {
 		TableName: aws_config.aws_table_name,
 		Limit:3 
 	}
-	var queries = null;
+	
+	//scan the first 3 items and render to screen
+	//save the LastEvaluatedKey to last_key for displayMoreRecipes funciton below
 	document.scan(params, function(err, data) {
 		if(err) console.log(err);
 		else {
-			console.log(data.Items);
-			res.render('index', {"recipes":data.Items});	
+			console.log(data.LastEvaluatedKey);
+			last_key = data.LastEvaluatedKey; //save LastEvaluatedKey
+			res.render('index', {"recipes":data.Items}); //render	
 		}
 	});
-	recipeCOunt = 0;
-	//queryResults = await colleciton.find().skip(0).limit(3).toArray();
-	//res.render('index');
 };
 
 
@@ -32,12 +33,20 @@ exports.displayRecipesInHomePage = async function(req, res) {
 * displayMoreRecipes - function to query next 3 recipes and display more recipes in home page
 */
 exports.displayMoreRecipes = async function(req, res) {
-	//db.connect();
-	//var collection = db.getdb().collection(collectionName);
-
-	recipeCount += 1;
-	//queryResults = await.collection.find().skip(recipeCount * 3).limit(3).toArray();
-	res.render('index');
+	//params - store TableName and ExclusiveStartKey and Limit Number of items get scanned	
+	var params = {
+		TableName: aws_config.aws_table_name,
+		Limit: 3,
+		ExclusiveStartKey: last_key
+	}
+	
+	document.scan(params, function(err, data) {
+		if(err) console.log(err);
+		else {
+			console.log(data.Items);
+			res.render('index', {"recipes":data.Items});	//render recipes
+		}
+	});
 };
 
 exports.displayAboutPage = async function(req, res) {
